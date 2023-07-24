350584a9c2bb8f5c72b084023eb5f773767ad764
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2023-07-24 12:38:09 +0100

    Adding files for training neural networks

---
+++ nn_ball_drop.py
@@ -0,0 +1,181 @@
+import random
+import torch
+from torch import nn
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# create a neural network to classify if ball_1 or ball_2 drops first or if they drop at the same time
+# input: x1, y1, x2, y2, dx1, dy1, dx2, dy2
+
+# Seed all random number generators for reproducibility
+seed = 6_345_789
+random.seed(seed)
+torch.manual_seed(seed)
+
+class NetBall(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(8, 16)
+        self.fc2 = nn.Linear(16, 16)
+        self.fc3 = nn.Linear(16, 3)
+        self.act1 = nn.ReLU()
+        self.act2 = nn.ReLU()
+
+    def forward(self, x):
+        x = self.act1(self.fc1(x))
+        x = self.act2(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class BallDataset(torch.utils.data.Dataset):
+    def __init__(self, df):
+        self.df = df
+        self.x = df[['x1', 'y1', 'x2', 'y2', 'dx1', 'dy1', 'dx2', 'dy2']].values
+        self.y = df[['ans']].values
+        # convert y to one-hot encoding 'same' = [1, 0, 0], 'ball_1' = [0, 1, 0], 'ball_2' = [0, 0, 1]
+        self.y = [[1, 0, 0] if y == 'same' else [0, 1, 0] if y == 'ball_1' else [0, 0, 1] for y in self.y]
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        if isinstance(idx, int):
+            idx = [idx]
+
+        batch_x = []
+        batch_y = []
+
+        for i in idx:
+            batch_x.append(self.x[i])
+            batch_y.append(self.y[i])
+
+        return torch.tensor(batch_x), torch.tensor(batch_y)
+
+
+net_ball = NetBall()
+
+loss_fn = nn.CrossEntropyLoss()
+
+optim_same = torch.optim.Adam(net_ball.parameters(), lr=0.0001)
+
+df = pd.read_csv('ball_drop/ball_drop.csv')
+
+indices = list(range(len(df)))
+random.shuffle(indices)
+train_indices = indices[:int(0.8 * len(indices))]
+val_indices = indices[int(0.8 * len(indices)):int(0.9 * len(indices))]
+test_indices = indices[int(0.9 * len(indices)):]
+
+# save train, val, test indices to csvs
+pd.DataFrame(train_indices).to_csv('indices/10k/train_indices.csv', index=False)
+pd.DataFrame(val_indices).to_csv('indices/10k/val_indices.csv', index=False)
+pd.DataFrame(test_indices).to_csv('indices/10k/test_indices.csv', index=False)
+
+train_dataset = BallDataset(df.iloc[train_indices])
+val_dataset = BallDataset(df.iloc[val_indices])
+test_dataset = BallDataset(df.iloc[test_indices])
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=True)
+
+
+def train(net, optim, loss_fn, train_loader, val_loader, epochs=10):
+    train_losses = []
+    val_losses = []
+    best_val_loss = float('inf')
+    best_model_dict = None
+    counter = 0
+    for epoch in range(epochs):
+        net.train()
+        epoch_loss = 0
+        for batch_x, batch_y in train_loader:
+            optim.zero_grad()
+            batch_y_hat = net(batch_x.float()).float()
+
+            # Apply softmax to get probabilities
+            # remove the superfluous dimensions for both dim(1)
+            batch_y_hat = batch_y_hat.squeeze(1)
+            batch_y = batch_y.squeeze(1)
+
+            batch_y = batch_y.squeeze()
+            loss = loss_fn(batch_y_hat, batch_y.float())  # .squeeze())
+            loss.backward()
+            optim.step()
+            epoch_loss += loss.item()
+        # Append average loss for this epoch
+        train_losses.append(epoch_loss / len(train_loader))
+
+        net.eval()
+        epoch_val_loss = 0
+        for batch_x, batch_y in val_loader:
+            batch_y_hat = net(batch_x.float())
+            # squeeze the superfluous dimensions for both dim(1)
+            batch_y_hat = batch_y_hat.squeeze(1)
+            batch_y = batch_y.squeeze(1)
+            loss = loss_fn(batch_y_hat, batch_y.float())  # .squeeze()
+            epoch_val_loss += loss.item()
+        # Append average loss for this epoch
+        val_losses.append(epoch_val_loss / len(val_loader))
+
+        # print losses to 4 decimal places
+        print(f'Epoch {epoch} : train loss = {train_losses[-1]:.4f}, val loss = {val_losses[-1]:.4f}')
+        # Save model if val loss is lower than the previous lowest val loss
+        if val_losses[-1] < best_val_loss:
+            best_val_loss = val_losses[-1]
+            best_model_dict = net.state_dict()
+
+        # Early stopping
+        if epoch > 5 and val_losses[-1] > val_losses[-2]:
+            counter += 1
+            # stop if the last 5 val losses are all higher than the previous
+            # or the last 5 are within 0.001 of each other
+
+            if counter > 5 or min(val_losses[-5:]) > best_val_loss:
+                print('Early stopping at epoch :', epoch)
+                break
+
+        else:
+            counter = 0
+
+
+    # Save best model
+    net.load_state_dict(best_model_dict)
+    # save the best model at the end of training
+    torch.save(net, 'ball_drop/best_model_ball.pt')
+
+    plt.plot(train_losses, label='train')
+    plt.plot(val_losses, label='val')
+    plt.legend()
+    plt.show()
+
+
+train(net_ball, optim_same, loss_fn, train_loader, val_loader, epochs=1000)
+
+
+def test(net, test_loader):
+    net.eval()
+    correct = 0
+    total = 0
+    confusion_matrix = torch.zeros(3, 3)
+    with torch.no_grad():
+        for batch_x, batch_y in test_loader:
+            batch_y_hat = net(batch_x.float()).squeeze(1)
+            batch_y = batch_y.squeeze(1)
+            correct += torch.eq(torch.argmax(batch_y_hat, dim=1), torch.argmax(batch_y, dim=1)).sum().item()
+            total += len(batch_y)
+            # update confusion matrix
+            for t, p in zip(torch.argmax(batch_y, dim=1), torch.argmax(batch_y_hat, dim=1)):
+                confusion_matrix[t.long(), p.long()] += 1
+            # print(confusion_matrix)
+
+            # calculate accuracy per class
+            # for i in range(3):
+
+    print(f'Test accuracy: {correct / total}')
+    print(confusion_matrix/confusion_matrix.sum(1).view(-1, 1))
+
+
+test(net_ball, test_loader)
---
+++ nn_freefall.py
@@ -0,0 +1,202 @@
+import random
+import torch
+from torch import nn
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Create a neural network to predict the vertical velocity and position of a falling object at time t given its initial x, y position and x, y velocity
+# Input: x_0, y_0, dx_0, dy_0, t_1
+class Netdiff(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(5, 16)
+        self.fc2 = nn.Linear(16, 16)
+        self.fc3 = nn.Linear(16, 1)
+        self.act1 = nn.ReLU()
+        self.act2 = nn.ReLU()
+
+    def forward(self, x):
+        x = self.act1(self.fc1(x))
+        x = self.act2(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class Netsame(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(5, 16)
+        self.fc2 = nn.Linear(16, 1)
+        self.act = nn.ReLU()
+
+    def forward(self, x):
+        x = self.act(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+class FreeFallDataset(torch.utils.data.Dataset):
+    def __init__(self, df):
+        self.df = df
+        self.x = df[['x_0', 'y_0', 'dx_0', 'dy_0', 't_1']].values
+        self.y = df[['y_1']].values
+
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        if isinstance(idx, int):
+            idx = [idx]
+
+        batch_x = []
+        batch_y = []
+
+        for i in idx:
+            batch_x.append(self.x[i])
+            batch_y.append(self.y[i])
+
+        return torch.tensor(batch_x), torch.tensor(batch_y)
+
+
+net_diff = Netdiff()
+net_same = Netsame()
+
+loss_fn_same = nn.MSELoss()
+loss_fn_diff = nn.MSELoss()
+
+optim_same = torch.optim.Adam(net_same.parameters(), lr=0.001)
+optim_diff = torch.optim.Adam(net_diff.parameters(), lr=0.001)
+
+df_same = pd.read_csv('freefall/examples_same_steps.csv')
+indices = list(range(len(df_same)))
+random.shuffle(indices)
+train_indices = indices[:int(0.8 * len(indices))]
+val_indices = indices[int(0.8 * len(indices)):int(0.9 * len(indices))]
+test_indices = indices[int(0.9 * len(indices)):]
+
+train_dataset_same = FreeFallDataset(df_same.iloc[train_indices])
+val_dataset_same = FreeFallDataset(df_same.iloc[val_indices])
+test_dataset_same = FreeFallDataset(df_same.iloc[test_indices])
+
+batch_size = 32
+data_loader_train_same = torch.utils.data.DataLoader(train_dataset_same, batch_size=batch_size, shuffle=True)
+data_loader_val_same = torch.utils.data.DataLoader(val_dataset_same, batch_size=batch_size, shuffle=True)
+data_loader_test_same = torch.utils.data.DataLoader(test_dataset_same, batch_size=batch_size, shuffle=True)
+
+df_diff = pd.read_csv('freefall/examples_diff_steps.csv')
+
+train_dataset_diff = FreeFallDataset(df_diff.iloc[train_indices])
+val_dataset_diff = FreeFallDataset(df_diff.iloc[val_indices])
+test_dataset_diff = FreeFallDataset(df_diff.iloc[test_indices])
+
+data_loader_train_diff = torch.utils.data.DataLoader(train_dataset_diff, batch_size=batch_size, shuffle=True)
+data_loader_val_diff = torch.utils.data.DataLoader(val_dataset_diff, batch_size=batch_size, shuffle=True)
+data_loader_test_diff = torch.utils.data.DataLoader(test_dataset_diff, batch_size=batch_size, shuffle=True)
+
+
+def evaluate(net, dataloader, loss_fn):
+    with torch.no_grad():
+        loss = 0
+        for x, y in dataloader:
+            y_pred = net(x.float())
+            loss += loss_fn(y_pred, y.float())
+        return loss / len(dataloader.dataset)
+
+
+def train(net, optim, loss_fn, train_dataloader, val_dataloader, epochs=100):
+    best_val_loss = float('inf')
+    best_state_dict = None
+    count = 0
+    train_losses = []
+    val_losses = []
+    for epoch in range(epochs):
+        train_loss = 0
+        for x, y in train_dataloader:
+            optim.zero_grad()
+            y_pred = net(x.float())
+            loss = loss_fn(y_pred, y.float())
+            loss.backward()
+            optim.step()
+            train_loss += loss.item()
+
+        val_loss = evaluate(net, val_dataloader, loss_fn)
+        train_losses.append(train_loss / len(train_dataloader))
+        val_losses.append(val_loss)
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            best_state_dict = net.state_dict()
+            count = 0
+        else:
+            count += 1
+        if count > 5:
+            print(f'Early stopping at epoch {epoch}')
+            break
+
+        if epoch % 10 == 0:
+            print(f'Epoch {epoch} training loss: {train_loss / len(train_dataloader):.4f}')
+            print(f'Validation loss: {val_loss:.4f}')
+
+    net.load_state_dict(best_state_dict)
+
+    # print(f'Test loss: {evaluate(net, data_loader_test, loss_fn):.4f}')
+
+    plt.figure()
+    plt.plot(train_losses, label='train')
+    plt.plot(val_losses, label='val')
+    plt.title(f'Losses for {net.__class__.__name__} NN model')
+    plt.legend()
+    plt.show()
+    return net
+
+
+print('Creating same network')
+same_args = {'net': net_same, 'optim': optim_same, 'loss_fn': loss_fn_same, 'train_dataloader': data_loader_train_same,
+             'val_dataloader': data_loader_val_same, 'epochs': 100}
+
+print('Creating different network')
+diff_args = {'net': net_diff, 'optim': optim_diff, 'loss_fn': loss_fn_diff, 'train_dataloader': data_loader_train_diff,
+             'val_dataloader': data_loader_val_diff, 'epochs': 100}
+
+print('\nTraining same network')
+net_same = train(**same_args)
+
+print('\nTraining different network')
+net_diff = train(**diff_args)
+
+torch.save(net_same, 'freefall/freefall_same.pt')
+torch.save(net_diff, 'freefall/freefall_diff.pt')
+
+# Now load examples_test.csv and run the networks on it
+df_test = pd.read_csv('freefall/examples_tests.csv')
+test_dataset = FreeFallDataset(df_test)
+test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+print('\nEvaluating same network')
+print(f'Test loss: {evaluate(net_same, test_dataloader, loss_fn_same):.4f}')
+
+print('\nEvaluating different network')
+print(f'Test loss: {evaluate(net_diff, test_dataloader, loss_fn_diff):.4f}')
+
+# Now run the networks on the test data and plot predicted vs actual
+with torch.no_grad():
+    for x, y in test_dataloader:
+        y_pred_same = net_same(x.float())
+        y_pred_diff = net_diff(x.float())
+        break
+
+plt.figure()
+plt.scatter(y, y_pred_same, label='same')
+plt.scatter(y, y_pred_diff, label='diff')
+plt.plot([0, 1], [0, 1], color='black')
+# plot a diagonal line to show perfect correlation
+plt.plot(y[:, 0, 0], y[:, 0, 0], color='black')
+plt.title('Predicted vs actual for test data set using same and different networks')
+
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
+
+plt.legend()
+plt.show()
